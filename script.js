18d2fe2ae8329808738b1fa8f8775180786b7e08
katherine-j-c-s <kathijcs@gmail.com>
2022-08-27 13:33:59 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const menubtn = document.querySelector(".menubtn")
+const options = document.querySelector(".options");
+const burger = document.querySelector(".burger");
+const cross = document.querySelector(".cross");
+
+const socialmedia = document.querySelector(".socialmedia");
+const facebookLink = document.querySelector("#facebookLink")
+const twiterLink = document.querySelector("#twiterLink")
+const instaLink = document.querySelector("#instaLink")
+
+const soundtracks = document.querySelector("#soundtracks");
+
+if (window.innerWidth < 800) {
+    
+    burger.classList.toggle("hidden");
+    
+    if (window.location.href == "file:///C:/Users/AeroCool/Documents/katherine/joan/index.html") {
+        console.log("esto es home")
+        soundtracks.width = 600;
+    }
+    else{
+        facebookLink.style.color = "black";
+        twiterLink.style.color = "black";
+        instaLink.style.color = "black";
+        console.log()
+    }
+}
+menubtn.addEventListener("click", function() {
+    console.log("hizo click")
+    burger.classList.toggle("hidden");
+    cross.classList.toggle("hidden");
+    options.classList.toggle("show-options");
+    socialmedia.classList.toggle("show-options");
+})
